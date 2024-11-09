78263731ffd70542888f00dce7ca5b4ab5cd9901
shahnoor7960 <samdhanicommunication@gmail.com>
2024-11-10 02:36:58 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var button = document.getElementById('toggle-skills');
+var skills = document.getElementById('skills');
+button.addEventListener('click', function () {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+    }
+    else {
+        skills.style.display = 'none';
+    }
+});
